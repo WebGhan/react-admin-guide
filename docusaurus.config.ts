02b7b1f1a2b908e55ae68b256e74937ee3be0a77
GaoHan <869912332@qq.com>
2026-04-17 01:22:50 +0800

    fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/WebGhan/Linian-React-Admin-Guide/tree/main/',
+            'https://github.com/WebGhan/react-admin-guide/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -73,7 +73,7 @@ const config: Config = {
         //   label: '文档',
         // },
         {
-          href: 'https://github.com/WebGhan/Linian-React-Admin-Guide',
+          href: 'https://github.com/WebGhan/react-admin-guide',
           label: 'GitHub',
           position: 'right',
         },
